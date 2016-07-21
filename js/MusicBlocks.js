7b0ddc7698ae4f1f3a8dc81ee3d36412b624f396
Cammac7 <cameron@Camerons-MacBook-Pro.local>
2016-07-21 16:42:59 -0400

    Added left column of new blocks

--- js/MusicBlocks.js
+++ js/MusicBlocks.js
@@ -3,26 +3,69 @@ $(document).ready(function() {
         $('#lromp3').toggle();
         $('#jealousmp3').hide();
         $('#girlcrushmp3').hide();
+        $('#bubblemp3').hide();
+        $('#glassmp3').hide();
+        
 		$('#loverunsoutpdf').show();
         $('#jealouspdf').hide();
-        $('#girlcrushpdf').hide();  
+        $('#girlcrushpdf').hide();
+        $('#glassleft').hide();
+        $('#bubbleleft').hide();
 	});
     
     $('#jealousClick').on('click', function(){
         $('#jealousmp3').toggle();
         $('#lromp3').hide();
         $('#girlcrushmp3').hide();
+        $('#bubblemp3').hide();
+        $('#glassmp3').hide();
+        
 		$('#loverunsoutpdf').hide();
         $('#jealouspdf').show();
-        $('#girlcrushpdf').hide();  
+        $('#girlcrushpdf').hide();
+        $('#glassleft').hide();
+        $('#bubbleleft').hide();
 	});
     
     $('#girlcrushClick').on('click', function(){
         $('#jealousmp3').hide();
         $('#lromp3').hide();
         $('#girlcrushmp3').toggle();
+        $('#bubblemp3').hide();
+        $('#glassmp3').hide();
+        
 		$('#loverunsoutpdf').hide();
         $('#jealouspdf').hide();
-        $('#girlcrushpdf').show();  
+        $('#girlcrushpdf').show();
+        $('#glassleft').hide();
+        $('#bubbleleft').hide();
+	});
+    
+    $('#bubbleClick').on('click', function(){
+        $('#jealousmp3').hide();
+        $('#lromp3').hide();
+        $('#girlcrushmp3').hide();
+        $('#bubblemp3').toggle();
+        $('#glassmp3').hide();
+        
+		$('#loverunsoutpdf').hide();
+        $('#jealouspdf').hide();
+        $('#girlcrushpdf').hide();
+        $('#glassleft').hide();
+        $('#bubbleleft').show();
+	});
+    
+    $('#glassClick').on('click', function(){
+        $('#jealousmp3').hide();
+        $('#lromp3').hide();
+        $('#girlcrushmp3').hide();
+        $('#bubblemp3').hide();
+        $('#glassmp3').toggle();
+        
+		$('#loverunsoutpdf').hide();
+        $('#jealouspdf').hide();
+        $('#girlcrushpdf').hide();
+        $('#glassleft').show();
+        $('#bubbleleft').hide();
 	});
 });
