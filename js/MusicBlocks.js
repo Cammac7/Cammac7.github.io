6c27f196b70d7aa2e99b2b3a9d84f157661ff833
Cammac7 <cameron@Camerons-MacBook-Pro.local>
2016-07-21 16:07:23 -0400

    Added no index +JS
    Added no index for googlebot.
    Added other sections of music blocks hide javascript

--- js/MusicBlocks.js
+++ js/MusicBlocks.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
 	$('#lroClick').on('click', function(){
         $('#lromp3').toggle();
+        $('#jealousmp3').hide();
+        $('#girlcrushmp3').hide();
 		$('#loverunsoutpdf').show();
         $('#jealouspdf').hide();
         $('#girlcrushpdf').hide();  
+	});
+    
+    $('#jealousClick').on('click', function(){
+        $('#jealousmp3').toggle();
+        $('#lromp3').hide();
+        $('#girlcrushmp3').hide();
+		$('#loverunsoutpdf').hide();
+        $('#jealouspdf').show();
+        $('#girlcrushpdf').hide();  
+	});
+    
+    $('#girlcrushClick').on('click', function(){
+        $('#jealousmp3').hide();
+        $('#lromp3').hide();
+        $('#girlcrushmp3').toggle();
+		$('#loverunsoutpdf').hide();
+        $('#jealouspdf').hide();
+        $('#girlcrushpdf').show();  
 	});
 });
